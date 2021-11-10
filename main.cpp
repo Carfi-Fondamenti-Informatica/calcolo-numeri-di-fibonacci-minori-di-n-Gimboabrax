85454932ff6f130fc6bbebbd3ab9e1c080c0ae12
Abrammm <91841366+Gimboabrax@users.noreply.github.com>
2021-11-10 21:17:09 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0,m=1,r=0,a=1;
+    cin >> n;
+    cout << a << endl;
+    cout << m << endl;
+    for(int i=2; i<n; i++) {
+        r = m + a;
+        if(r<=n) {
+            cout << r << endl;
+        }
+        a = m;
+        m = r;
+
+    }
+
+    return 0;
 }
